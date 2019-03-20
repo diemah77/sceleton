ea45a5db058e71b553612610751f09debe9d326a
Dieter Frei <dfrei84@gmail.com>
2019-03-20 13:54:31 +0100

    move shop auth middleware to route servide provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -51,9 +51,16 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
+        $middlewares = ['web'];
+
+        if (app()->environment() == 'production')
+        {
+            array_push($middlewares, 'auth.shop');
+        }
+
+        Route::middleware($middlewares)
+            ->namespace($this->namespace)
+            ->group(base_path('routes/web.php'));
     }
 
     /**
@@ -66,8 +73,8 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
     }
 }
